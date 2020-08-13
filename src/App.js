2b259558abb05faabeb45ec84b3abc717390f70a
Tereit <morten_mulvik@hotmail.com>
2020-08-13 11:32:57 +0200

    Added bootstrap and changed the menu

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './Screens/Home/Home';
 import Landingscreen from './Screens/LandingScreen/landingscreen';
 import Newbetting from './Screens/NewBetting/newbetting';
--- src/Components/Menu/menu.js
+++ src/Components/Menu/menu.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './menu.css';
+import Navbar from 'react-bootstrap/Navbar';
+import { Nav } from 'react-bootstrap';
 
 function Menu(props) {
     return (
-        <nav>
-            <h1 className='AppName'>Fantasy League</h1>
-            <ul>
-                <li><Link to='/'>Landingpage</Link></li>
-                <li><Link to='/home'>Home</Link></li>
-                <li><Link to='/newbet'>New Bet</Link></li>
-            </ul>
-        </nav>
+        <Navbar bg="dark" variant="dark" expand="lg">
+            <Navbar.Brand as={Link} to="/">Fantasy League</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link as={Link} to="/home">Home</Nav.Link>
+                    <Nav.Link as={Link} to="/newbet">New Bet</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
     );
 }
 
